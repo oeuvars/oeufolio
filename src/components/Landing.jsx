055faf4f0b77d4a9c2b19b42fa435d84588993fb
oeuvars <www.anuragniall@gmail.com>
2023-06-30 01:06:21 +0530

    Font Improved

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -33,7 +33,7 @@ const Landing = ({ setSelectedPage }) => {
             <div className="z-30 basis-2/5 mt-5 md:mt-24">
                 {/*Heading Section */}
                 <motion.div initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants = {{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 }}} className="phone:mt-10 md:mt-0">
-                    <p className="text-5xl md:text-7xl font-medium font-script z-10 text-orange-400 text-center md:text-start">Anurag Das</p>
+                    <p className="text-5xl md:text-7xl font-medium font-chloe z-10 text-orange-400 text-center md:text-start">Anurag Das</p>
                     <p className="mt-10 mb-7 text-center text-black font-outfit text-base font-medium md:text-start lg:text-lg">Full Stack Web Development (MERN), also Solid.js, Web Design and Stuffs.</p>
                 </motion.div>
 
