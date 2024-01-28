f8d26fc3083292fff8981c19626d4c70746221aa
oeuavrs <www.anuragniall@gmail.com>
2024-01-28 13:35:26 +0530

    some fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
 
 export const metadata: Metadata = {
   title: "Oeuvars",
