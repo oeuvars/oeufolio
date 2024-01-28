decb1ef92f5d32a72ab3160dd2ca07ca41c248a1
oeuavrs <www.anuragniall@gmail.com>
2024-01-28 17:03:46 +0530

    hook error fixed hopefully 4

--- src/components/BackgroundRippleEffect.tsx
+++ src/components/BackgroundRippleEffect.tsx
@@ -3,14 +3,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { cn } from "@/lib/utils";
-import { AnimationControls } from 'framer-motion';
-
-type ClickedCell = [number, number];
-interface PatternCellProps {
-  controls: AnimationControls;
-  cellClassName?: string;
-  onClick: () => void;
-}
 
 export const BackgroundCellAnimation = () => {
   const variants = {
@@ -25,7 +17,7 @@ return (
          <img src="/icons/Logo.svg" alt="" className="phone:w-12 phone:h-12 lg:w-20 lg:h-20 my-auto"/>
          <h1 className="phone:text-5xl lg:text-7xl font-playfair p-1 my-auto bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 via-stone-400 to-neutral-500 tracking-tighter">Oeuvars</h1>
       </motion.div>
-      <motion.div transition={{ ease: "easeOut", duration: 1.2 }} variants={variants} initial="hidden" animate="enter" className="phone:text-2xl lg:text-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 via-stone-300 to-neutral-400 tracking-tight text-center font-parisienne py-1">Anurag Das, Lemme land g.</motion.div>
+      <motion.div transition={{ ease: "easeOut", duration: 1.2 }} variants={variants} initial="hidden" animate="enter" className="phone:text-2xl lg:text-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 via-stone-300 to-neutral-400 tracking-tight text-center font-parisienne py-1">Anurag Das. Lemme land g.</motion.div>
       <motion.div className="flex w-full phone:gap-2 lg:gap-0 mt-2 " transition={{ ease: "easeOut", duration: 1.3 }} variants={variants} initial="hidden" animate="enter">
         <input
           className="bg-[#222222] text-[#FAFAFA] px-6 py-3 w-full rounded-md outline-none focus:outline-2 focus:outline focus:outline-[#333333]"
@@ -46,9 +38,7 @@ return (
 
 const BackgroundCellCore = () => {
 const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
 const ref = useRef<any>(null);
-
 const handleMouseMove = (event: any) => {
   const rect = ref.current && ref.current.getBoundingClientRect();
   setMousePosition({
@@ -95,53 +85,7 @@ return (
 );
 };
 
-const usePatternAnimation = (clickedCell: ClickedCell | null, rowIdx: number, colIdx: number) => {
-  const controls = useAnimation();
-
-  useEffect(() => {
-    if (clickedCell) {
-      const distance = Math.sqrt(
-        Math.pow(clickedCell[0] - rowIdx, 2) +
-        Math.pow(clickedCell[1] - colIdx, 2)
-      );
-      controls.start({
-        opacity: [0, 1 - distance * 0.1, 0],
-        transition: { duration: distance * 0.2 },
-      });
-    }
-  }, [clickedCell]);
-
-  return controls;
-};
-
-const PatternCell:React.FC<PatternCellProps> = ({ controls, cellClassName, onClick }) => {
-  return (
-    <div
-      className={cn(
-        "bg-transparent border-l border-b border-neutral-600",
-        cellClassName
-      )}
-      onClick={onClick}
-    >
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        whileHover={{
-          opacity: [0, 1, 0.5],
-        }}
-        transition={{
-          duration: 0.5,
-          ease: "backOut",
-        }}
-        animate={controls}
-        className="bg-purple-600/30 h-12 w-12"
-      ></motion.div>
-    </div>
-  );
-};
-
-const Pattern = ({className, cellClassName}: {className?: string; cellClassName?: string}) => {
+function Pattern({className, cellClassName}: {className?: string; cellClassName?: string}) {
   const x = new Array(47).fill(0);
   const y = new Array(30).fill(0);
   const matrix = x.map((_, i) => y.map((_, j) => [i, j]));
@@ -155,15 +99,45 @@ const Pattern = ({className, cellClassName}: {className?: string; cellClassName?
           className="flex flex-col  relative z-20 border-b"
         >
           {row.map((column, colIdx) => {
-            const controlsArray = row.map((_, colIdx) => usePatternAnimation(clickedCell, rowIdx, colIdx));
+            const controls = useAnimation();
+
+            useEffect(() => {
+              if (clickedCell) {
+                const distance = Math.sqrt(
+                  Math.pow(clickedCell[0] - rowIdx, 2) +
+                    Math.pow(clickedCell[1] - colIdx, 2)
+                );
+                controls.start({
+                  opacity: [0, 1 - distance * 0.1, 0],
+                  transition: { duration: distance * 0.2 },
+                });
+              }
+            }, [clickedCell]);
 
             return (
-              <PatternCell
+              <div
                 key={`matrix-col-${colIdx}`}
-                controls={controlsArray[colIdx]}
-                cellClassName={cellClassName}
+                className={cn(
+                  "bg-transparent border-l border-b border-neutral-600",
+                  cellClassName
+                )}
                 onClick={() => setClickedCell([rowIdx, colIdx])}
-              />
+              >
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                  }}
+                  whileHover={{
+                    opacity: [0, 1, 0.5],
+                  }}
+                  transition={{
+                    duration: 0.5,
+                    ease: "backOut",
+                  }}
+                  animate={controls}
+                 className="bg-purple-600/30 h-12 w-12"
+                ></motion.div>
+              </div>
             );
           })}
         </div>
